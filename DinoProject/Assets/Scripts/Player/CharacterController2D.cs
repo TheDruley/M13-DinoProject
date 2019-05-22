0308307dad31d12062aabba4ed3236950eee41b6
edgar <edgar@DESKTOP-HJ8S6UQ>
2019-05-22 20:13:42 +0200

    Added Animation Transitions

--- DinoProject/Assets/Scripts/Player/CharacterController2D.cs
+++ DinoProject/Assets/Scripts/Player/CharacterController2D.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
+    public Animator animator;
     [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
     [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
@@ -142,6 +143,9 @@ public class CharacterController2D : MonoBehaviour
     {
         //if (m_Grounded && jumpButtonPressed)
     }
+
+    
+    
 
 
     private void FlipCharacter()
