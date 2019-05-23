1a2d389bd949eb3c4170e9ed5a17caeaf6c5e935
PolGV98 <50147302+PolGV98@users.noreply.github.com>
2019-05-23 19:12:53 +0200

    hola
    problemas porq soy imbecil :)

--- DinoProject/Assets/Scripts/PinchoController.cs
+++ DinoProject/Assets/Scripts/PinchoController.cs
@@ -7,8 +7,8 @@ public class PinchoController : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    
-    
+    GameObject dinoPlayer;
+
 
     void Start()
     {
@@ -25,7 +25,12 @@ public class PinchoController : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Destroy(dinoPlayer);
+            Vector2 vector = new Vector2(0, 0);
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate(dinoPlayer, vector, transform.rotation);
+            dinoPlayer = GameObject.FindWithTag("Player");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             //Destroy(col);
         }
     }
