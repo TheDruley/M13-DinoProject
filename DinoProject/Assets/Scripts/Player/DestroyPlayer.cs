86af1c09db4117992d264cf179d87607281358cc
edgar <edgar@DESKTOP-HJ8S6UQ>
2019-05-27 17:03:45 +0200

    Fixed Spikes and some animations changed

--- DinoProject/Assets/Scripts/Player/DestroyPlayer.cs
+++ DinoProject/Assets/Scripts/Player/DestroyPlayer.cs
@@ -27,11 +27,12 @@ public class DestroyPlayer : MonoBehaviour
         if (col.gameObject.tag.Equals("Muerte"))
         {
             Instantiate(bloodSplash,gameObject.transform.position, Quaternion.identity);
+            gameObject.transform.position = spawnPoint.transform.position;
+            
             //spawnPoint.GetComponent<SpawnerScript>().SpawnPlayer(gameObject);
             //StartCoroutine(waiter(1, gameObject));
             //Destroy(gameObject);
                 
-            gameObject.transform.position = spawnPoint.transform.position;
         }
     }
 
